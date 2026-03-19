8e6c4b6a5d6cba361a2779d07fc1cef486b61ffe
KolManis <nikola.ilyin03@yandex.ru>
2026-03-19 17:45:03 +0500

    add basic gRPC server skeleton

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	grpcPort = 50051
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Printf("failed to listen: %v\n", err)
+		return
+	}
+	defer func() {
+		if cerr := lis.Close(); cerr != nil {
+			log.Printf("failed to close listener: %v\n", cerr)
+		}
+	}()
+
+	// Создаем gRPC сервер
+	s := grpc.NewServer()
+
+	// Регистрируем наш сервис
+	//TODO
+	// sessionManager :=
+	// api :=
+
+	//telegramv1.RegisterTelegramServiceServer(s, api)
+
+	// Включаем рефлексию для отладки
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("gRPC server listening on %d\n", grpcPort)
+		err = s.Serve(lis)
+		if err != nil {
+			log.Printf("failed to serve: %v\n", err)
+			return
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down gRPC server...")
+	s.GracefulStop()
+	log.Println("Server stopped")
+}
